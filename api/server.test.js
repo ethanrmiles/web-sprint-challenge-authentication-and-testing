70a3d3105a4e2d4d3b1cec89596a3a9e2787acac
Ethan Miles <ethanmilescs@gmail.com>
2022-04-29 11:23:19 -0500

    Switched up Knex version, added base server sanity check ( GET :3300/ res = 'server is up and running!' )

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,14 @@
-// Write your tests here
+const jokesRouter = require('./jokes/jokes-router')
+const authRouter = require('./auth/auth-router')
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const jokesData = require('./jokes/jokes-data')
+
+
 test('sanity', () => {
   expect(true).toBe(true)
+})
+
+describe('tests relating to the jokes endpoint', () => {
+  test()
 })
